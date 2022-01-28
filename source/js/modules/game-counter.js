a40a49b8b96e1788935b95951535479a5fa577e3
Polina Prokofieva <polina.prokofieva@gmail.com>
2022-01-28 20:35:01 +0200

    Исправила направление отсчёта

--- source/js/modules/game-counter.js
+++ source/js/modules/game-counter.js
@@ -8,14 +8,14 @@ let timer;
 const convertToTwoDigits = number => number < 10 ? `0${number}` : number;
 
 const draw = () => {
-  const totalSecondsLeft = MAX_DURATION - Math.ceil((Date.now() - start) / 1000);
+  const totalSecondsLeft = Math.floor((Date.now() - start) / 1000);
   const seconds = totalSecondsLeft % 60;
   const minutes = (totalSecondsLeft - seconds) / 60;
 
   minutesNode.innerText = convertToTwoDigits(minutes);
   secondsNode.innerText = convertToTwoDigits(seconds);
 
-  if(totalSecondsLeft <= 0) {
+  if(totalSecondsLeft >= MAX_DURATION) {
     clearInterval(timer);
   }
 }
