b71aec9506b2e375277f8e149a965e9fec39cf85
Polina Prokofieva <polina.prokofieva@gmail.com>
2022-02-01 19:57:31 +0200

    Счётчики чисел

--- source/js/modules/game-counter.js
+++ source/js/modules/game-counter.js
@@ -1,25 +1,74 @@
-const MAX_DURATION = 5 * 60;
+const MAX_DURATION = 300;
+const FPS = 12;
+
+const prizes = {
+  journeys: {
+    amount: [1, 3],
+    start: 2000
+  },
+  cases: {
+    amount: [1, 7],
+    start: 3000
+  },
+  codes: {
+    amount: [11, 900],
+    start: 4000
+  }
+};
+
 const minutesNode = document.querySelector('.game__minutes');
 const secondsNode = document.querySelector('.game__seconds');
 
-const start = Date.now();
-let timer;
+const gameCounter = () => {
+  const start = Date.now();
+  const timer = setInterval(() => { requestAnimationFrame(drawTime) }, 1000);
 
-const convertToTwoDigits = number => number < 10 ? `0${number}` : number;
+  const convertToTwoDigits = number => number < 10 ? `0${number}` : number;
 
-const draw = () => {
-  const totalSecondsLeft = Math.floor((Date.now() - start) / 1000);
-  const seconds = totalSecondsLeft % 60;
-  const minutes = (totalSecondsLeft - seconds) / 60;
+  const drawTime = () => {
+    const totalSecondsLeft = Math.floor((Date.now() - start) / 1000);
+    const seconds = totalSecondsLeft % 60;
+    const minutes = (totalSecondsLeft - seconds) / 60;
 
-  minutesNode.innerText = convertToTwoDigits(minutes);
-  secondsNode.innerText = convertToTwoDigits(seconds);
+    minutesNode.innerText = convertToTwoDigits(minutes);
+    secondsNode.innerText = convertToTwoDigits(seconds);
 
-  if(totalSecondsLeft >= MAX_DURATION) {
-    clearInterval(timer);
+    if(totalSecondsLeft >= MAX_DURATION) {
+      clearInterval(timer);
+    }
+  }
+};
+
+const prizesCounter = ({ amount, node }) => {
+  const numberNode = node.querySelector('.prizes__number');
+  const descriptionNode = node.querySelector('.prizes__desc');
+  const step = Math.max((amount[1] - amount[0]) / FPS, 1);
+  const timer = setInterval(() => { requestAnimationFrame(drawNumber) }, 1000 / FPS);
+  let counter = amount[0];
+  let roundedCounter = counter;
+
+  descriptionNode.classList.add('prizes__desc--active');
+
+  const drawNumber = () => {
+    numberNode.innerText = roundedCounter;
+    counter = counter + step;
+    roundedCounter = Math.floor(counter);
+
+    if(roundedCounter > amount[1]) {
+      clearInterval(timer);
+    }
   }
 }
 
-export default () => {
-  timer = setInterval(() => { requestAnimationFrame(draw) }, 1000);
-};
+const launchAllPrizesCounters = () => {
+  Object.keys(prizes)
+    .forEach(key => {
+      prizes[key].node = document.querySelector(`.prizes__item--${key}`);
+    });
+
+  Object.values(prizes).forEach(prize => {
+    setTimeout(() => { prizesCounter(prize); }, prize.start);
+  });
+}
+
+export {gameCounter, launchAllPrizesCounters};
